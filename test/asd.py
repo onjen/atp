0f85a6768c5b359d8822d1a48ed6286dfba8c474
Johannes Rothe <johannes.rothe@rwth-aachen.de>
2015-11-11 00:18:19 +0100

    try surf method

--- test/asd.py
+++ test/asd.py
@@ -2,6 +2,10 @@ import numpy as np
 import cv2
 from matplotlib import pyplot as plt
 
-img = cv2.imread('asd.jpg',cv2.IMREAD_COLOR)
-plt.imshow(img)
-plt.show()
+img = cv2.imread('pic.png',0)
+surf = cv2.xfeatures2d.SURF(400)
+# Find keypoints and descriptors directly
+kp, des = surf.detectAndCompute(img,None)
+len(kp)
+#plt.imshow(img)
+#plt.show()
