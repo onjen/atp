c2c0376bdb9a1f657d616f3e5ba6b949d6859f65
Johannes Rothe <johannes.rothe@rwth-aachen.de>
2015-11-13 17:08:19 +0100

    correct sift to sift_create

--- test/asd.py
+++ test/asd.py
@@ -3,9 +3,9 @@ import cv2
 from matplotlib import pyplot as plt
 
 img = cv2.imread('pic.png',0)
-surf = cv2.xfeatures2d.SURF(400)
+surf = cv2.xfeatures2d.SURF_create(400)
 # Find keypoints and descriptors directly
 kp, des = surf.detectAndCompute(img,None)
-len(kp)
+print(len(kp))
 #plt.imshow(img)
 #plt.show()
