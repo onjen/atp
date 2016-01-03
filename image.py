96f8623f57aebc8fb25156f8d64e5b79e30908b2
Johannes Rothe <johannes.rothe@rwth-aachen.de>
2016-01-03 15:35:34 +0100

    new class Image
    stores the keypoints, descriptors and brick_id
    and will be serialized with pickle

---
+++ image.py
@@ -0,0 +1,23 @@
+'''
+class image
+
+defines the image path, the keypoints, the descriptors
+and the associated brick
+'''
+class Image:
+    def __init__(self, keypoints, descriptors, filename, brick_id):
+        self.keypoints_array = pickle_keypoints(keypoints, descriptors)
+        self.filename = filename
+        self.brick_id = brick_id
+
+# format keypoints to use pickle to serialize keypoints and  descriptors
+def pickle_keypoints(keypoints, descriptors):
+    i = 0
+    temp_array = []
+    for point in keypoints:
+        temp = (point.pt, point.size, point.angle, point.response, point.octave,
+        point.class_id, descriptors[i])
+        ++i
+        temp_array.append(temp)
+    return temp_array
+
